015152f940ae2cad45fb2a588cac7e3b900bd160
Diego Santos <diegodeveloper42@gmail.com>
2022-02-20 18:10:20 -0300

    Finished header

--- js/main.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-  const nav = document.getElementById('navigation');
-  nav.classList.toggle('active');
-}
-const btnMobile = document.getElementById('btn-mobile');
-btnMobile.addEventListener('click', toggleMenu);
